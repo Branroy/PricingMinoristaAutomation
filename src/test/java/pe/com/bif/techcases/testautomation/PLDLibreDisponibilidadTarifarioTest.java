abcd605afbe42b8189d75d009a0b64956a0d0dc3
Roy Ortiz <branroy14@gmail.com>
2023-05-28 23:14:07 -0500

    Se culminó el data driver con base de datos para PLDLibreDisponibilidad se culmino la lógica

--- src/test/java/pe/com/bif/techcases/testautomation/PLDLibreDisponibilidadTarifarioTest.java
+++ src/test/java/pe/com/bif/techcases/testautomation/PLDLibreDisponibilidadTarifarioTest.java
@@ -16,100 +16,155 @@ import java.sql.*;
 
 
 @RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom(value="src/test/resources/testdata/PLDLibreDisponibilidadTarifario.csv")
+@UseTestDataFrom(value = "src/test/resources/testdata/PLDLibreDisponibilidadTarifario.csv")
 public class PLDLibreDisponibilidadTarifarioTest extends BaseConf {
 
-    String connectionString ="jdbc:sqlserver://bif3pricing20;Database=BIFPricing;TrustServerCertificate=true";
-    String login ="usr_princing";
-    String pass ="43#pr1@8";
+    String connectionString = "jdbc:sqlserver://bif3pricing20;Database=BIFPricing;TrustServerCertificate=true";
+    String login = "usr_princing";
+    String pass = "43#pr1@8";
 
 
     private String cargo;
-    private String escliente;
+    private String clientebif;
     private String segmentacion;
     private String modalidad;
     private String requieresustento;
 
+    private String divisa;
+    private String monto;
+    private String plazo;
+    private String nroplazo;
+
     @Before
-    public void navigate() { actor.attemptsTo(NavigateTo.signUpPage());
+    public void navigate() {
+        actor.attemptsTo(NavigateTo.signUpPage());
 
     }
 
     @Test
     public void CotizacionTarifarioPLD() {
 
-        String connectionString =
-                "jdbc:sqlserver://bif3pricing20;Database=BIFPricing;TrustServerCertificate=true";
+        if (modalidad.equalsIgnoreCase("REENGANCHE")) {
+            try {
+                Connection connection = DriverManager.getConnection(connectionString, login, pass);
+                System.out.println("Connection established.");
 
-        String login =
-                "usr_princing";
+                Statement mystatement = connection.createStatement();
+                ResultSet bd = mystatement.executeQuery(
+                        "SELECT * FROM (SELECT TOP 1 REGISTRO,PASSWORD FROM ORG_INTEGRANTE2 WHERE PRESENCIA = 1 AND LOGUEO = 1 AND ESTADO = 1 AND ESTADO_SOL_ACTIVACION = 'ACTIVO' AND DESC_CARGO = '"+cargo+"' ORDER BY NEWID()\n" +
+                                ") AS T1, (SELECT TOP(1) l.num_doc_identidad as NRODOCUMENTO, I.INGRESO_NETO AS INGRESOS, D.COD_CUENTA AS NROPAGARE, D.DIVISA AS DIVISA FROM CFG_LLE L INNER JOIN CFG_CLIENTES C ON L.num_doc_identidad = C.NRO_DOCUMENTO \n" +
+                                "JOIN CFG_INGRESOS I ON I.NUM_DOC_IDENTIDAD = L.num_doc_identidad\n" +
+                                "JOIN CFG_INFOADICIONAL_DESEMBOLSOS D ON D.NRO_DOCUMENTO = L.num_doc_identidad\n" +
+                                "WHERE L.tip_doc_identidad = '1' AND L.NUM_DOC_IDENTIDAD != ' ' AND C.RANGO_INGRESO != '0.0' AND C.CLIENTE_BIF = '"+clientebif+"' AND C.SEGMENTACION_RIESGOS = '"+segmentacion+"'\n" +
+                                "AND L.cliente_bif = '"+clientebif+"' AND L.Segmentacion ='"+segmentacion+"' AND I.TIP_DOC_IDENTIDAD = '1' ORDER BY NEWID()) AS T2,\n" +
+                                "(SELECT TOP(1) OPCION AS TIPORELACIONLABORAL FROM VLR_VARIABLE WHERE VARIABLE = 'TIPO_REL_LABORAL' AND COD_PRODUCTO = '1' ORDER BY NEWID()) AS T3");
 
-        String pass =
-                "43#pr1@8";
-
-
-        //String cargo = "GERENTE DE OFICINA";
-        try{
-            Connection connection = DriverManager.getConnection(connectionString,login,pass);
-            System.out.println("Connection established.");
-
-            Statement mystatement = connection.createStatement();
-
-            ResultSet bd = mystatement.executeQuery(
-                    "SELECT * FROM (SELECT TOP 1 REGISTRO,PASSWORD FROM ORG_INTEGRANTE2 WHERE PRESENCIA = 1 AND LOGUEO = 1 AND ESTADO = 1 AND ESTADO_SOL_ACTIVACION = 'ACTIVO' AND DESC_CARGO = 'REPRESENTANTE DE SERVICIO AL CLIENTE' ORDER BY NEWID()\n" +
-                            ") AS T1, (SELECT TOP(1) l.num_doc_identidad as NRODOCUMENTO, I.INGRESO_NETO AS INGRESOS, D.COD_CUENTA AS NROPAGARE, D.DIVISA AS DIVISA FROM CFG_LLE L INNER JOIN CFG_CLIENTES C ON L.num_doc_identidad = C.NRO_DOCUMENTO \n" +
-                            "JOIN CFG_INGRESOS I ON I.NUM_DOC_IDENTIDAD = L.num_doc_identidad\n" +
-                            "JOIN CFG_INFOADICIONAL_DESEMBOLSOS D ON D.NRO_DOCUMENTO = L.num_doc_identidad\n" +
-                            "WHERE L.tip_doc_identidad = '1' AND L.NUM_DOC_IDENTIDAD != ' ' AND C.RANGO_INGRESO != '0.0' AND C.CLIENTE_BIF = '0' AND C.SEGMENTACION_RIESGOS = '4'\n" +
-                            "AND L.cliente_bif = '0' AND L.Segmentacion ='4' AND I.TIP_DOC_IDENTIDAD = '1' ORDER BY NEWID()) AS T2");
-
-            while (bd.next()){
-                String usuario = bd.getString(1);
-                String password = bd.getString(2);
-                String nrodocumento = bd.getString(3);
-                double ingresoneto = bd.getDouble(4);
-                String nropagare = bd.getString(5);
-                String divisa = bd.getString(6);
-
-                actor.attemptsTo(
-                        LoginActions.usuario(usuario),
-                        LoginActions.password(password),
-                        LoginActions.entrar(),
-                        BandejaCotizacionesFDNActions.nuevacotizacion(),
-                        SolicitudNuevoActions.nrodocumento(nrodocumento),
-                        SolicitudNuevoActions.buscarcliente(),
-                        //SolicitudNuevoActions.nombrecliente(escliente),
-                        SolicitudNuevoActions.macroproducto("PLD"),
-                        SolicitudNuevoActions.producto("PTMO. LIBRE DISPONIBILIDAD"),
-                        SolicitudNuevoActions.modalidad(modalidad),
-                        SolicitudNuevoActions.nropagare(nropagare,modalidad),
-                        SolicitudNuevoActions.tipoingreso("INGRESO INDIVIDUAL"),
-                        SolicitudNuevoActions.relacionlaboral("MIXTA"),
-                        SolicitudNuevoActions.ingresonetoconsustento(ingresoneto,requieresustento),
-                        SolicitudNuevoActions.importe(divisa),
-                        SolicitudNuevoActions.montooperacion("30000.00"),
-                        SolicitudNuevoActions.garantia("SIN GARANTIA"),
-                        SolicitudNuevoActions.plazo("AÑO"),
-                        SolicitudNuevoActions.nroplazo("3"),
-                        SolicitudNuevoActions.opencalendar(),
-                        SolicitudNuevoActions.nextmonthcalendar(),
-                        SolicitudNuevoActions.okcalendar(),
-                        SolicitudNuevoActions.calculartasa()
-                    /*SolicitudNuevoActions.guardaroperacion(),
-                    SolicitudNuevoActions.confirmaroperacion()*/
-                );
+                while (bd.next()) {
+                    String usuario = bd.getString(1);
+                    String password = bd.getString(2);
+                    String nrodocumento = bd.getString(3);
+                    double ingresoneto = bd.getDouble(4);
+                    String nropagare = bd.getString(5);
+                    String divisa = bd.getString(6);
+                    String tiporelacionlaboral = bd.getString(7);
 
 
+                    actor.attemptsTo(
+                            LoginActions.usuario(usuario),
+                            LoginActions.password(password),
+                            LoginActions.entrar(),
+                            BandejaCotizacionesFDNActions.nuevacotizacion(),
+                            SolicitudNuevoActions.tipodocumento("DNI"),
+                            SolicitudNuevoActions.nrodocumento(nrodocumento),
+                            SolicitudNuevoActions.buscarcliente(),
+                            //SolicitudNuevoActions.nombrecliente(escliente),
+                            SolicitudNuevoActions.macroproducto("PLD"),
+                            SolicitudNuevoActions.producto("PTMO. LIBRE DISPONIBILIDAD"),
+                            SolicitudNuevoActions.modalidad(modalidad),
+                            SolicitudNuevoActions.nropagare(nropagare, modalidad),
+                            SolicitudNuevoActions.tipoingreso("INGRESO INDIVIDUAL"),
+                            SolicitudNuevoActions.relacionlaboral(tiporelacionlaboral),
+                            SolicitudNuevoActions.ingresonetoconsustento(ingresoneto, requieresustento),
+                            SolicitudNuevoActions.importe(divisa),
+                            SolicitudNuevoActions.montooperacion(monto),
+                            SolicitudNuevoActions.garantia("SIN GARANTIA"),
+                            SolicitudNuevoActions.plazo(plazo),
+                            SolicitudNuevoActions.nroplazo(nroplazo),
+                            SolicitudNuevoActions.opencalendar(),
+                            SolicitudNuevoActions.nextmonthcalendar(),
+                            SolicitudNuevoActions.okcalendar(),
+                            SolicitudNuevoActions.calculartasa(),
+                            SolicitudNuevoActions.guardaroperacion(),
+                            SolicitudNuevoActions.confirmaroperacion());
+
+                }
+            } catch (SQLException e) {
+                System.out.println("Error connection to the database");
+                e.printStackTrace();
             }
 
-        } catch (SQLException e){
-            System.out.println("Error connection to the database");
-            e.printStackTrace();
+
+        } else {
+            try {
+                Connection connection = DriverManager.getConnection(connectionString, login, pass);
+                System.out.println("Connection established.");
+
+                Statement mystatement = connection.createStatement();
+                ResultSet bd = mystatement.executeQuery(
+                        "SELECT * FROM (SELECT TOP 1 REGISTRO,PASSWORD FROM ORG_INTEGRANTE2 WHERE PRESENCIA = 1 AND LOGUEO = 1 AND ESTADO = 1 AND ESTADO_SOL_ACTIVACION = 'ACTIVO' AND DESC_CARGO = '"+cargo+"' ORDER BY NEWID()\n" +
+                                ") AS T1, (SELECT TOP(1) l.num_doc_identidad as NRODOCUMENTO, I.INGRESO_NETO AS INGRESOS FROM CFG_LLE L INNER JOIN CFG_CLIENTES C ON L.num_doc_identidad = C.NRO_DOCUMENTO \n" +
+                                "JOIN CFG_INGRESOS I ON I.NUM_DOC_IDENTIDAD = L.num_doc_identidad\n" +
+                                "WHERE L.tip_doc_identidad = '1' AND L.NUM_DOC_IDENTIDAD != ' ' AND C.RANGO_INGRESO != '0.0' AND C.CLIENTE_BIF = '"+clientebif+"' AND C.SEGMENTACION_RIESGOS = '"+segmentacion+"'\n" +
+                                "AND L.cliente_bif = '"+clientebif+"' AND L.Segmentacion ='"+segmentacion+"' AND I.TIP_DOC_IDENTIDAD = '1' AND L.num_doc_identidad LIKE '72%' ORDER BY NEWID()) AS T2, \n" +
+                                "(SELECT TOP(1) OPCION AS TIPORELACIONLABORAL FROM VLR_VARIABLE WHERE VARIABLE = 'TIPO_REL_LABORAL' AND COD_PRODUCTO = '1' ORDER BY NEWID()) as T3");
+
+                while (bd.next()) {
+                    String usuario = bd.getString(1);
+                    String password = bd.getString(2);
+                    String nrodocumento = bd.getString(3);
+                    double ingresoneto = bd.getDouble(4);
+                    String nropagare = "";
+                    String tiporelacionlaboral = bd.getString(5);
+
+                    actor.attemptsTo(
+                            LoginActions.usuario(usuario),
+                            LoginActions.password(password),
+                            LoginActions.entrar(),
+                            BandejaCotizacionesFDNActions.nuevacotizacion(),
+                            SolicitudNuevoActions.tipodocumento("DNI"),
+                            SolicitudNuevoActions.nrodocumento(nrodocumento),
+                            SolicitudNuevoActions.buscarcliente(),
+                            //SolicitudNuevoActions.nombrecliente(escliente),
+                            SolicitudNuevoActions.macroproducto("PLD"),
+                            SolicitudNuevoActions.producto("PTMO. LIBRE DISPONIBILIDAD"),
+                            SolicitudNuevoActions.modalidad(modalidad),
+                            SolicitudNuevoActions.nropagare(nropagare, modalidad),
+                            SolicitudNuevoActions.tipoingreso("INGRESO INDIVIDUAL"),
+                            SolicitudNuevoActions.relacionlaboral(tiporelacionlaboral),
+                            SolicitudNuevoActions.ingresonetoconsustento(ingresoneto, requieresustento),
+                            SolicitudNuevoActions.importe(divisa),
+                            SolicitudNuevoActions.montooperacion(monto),
+                            SolicitudNuevoActions.garantia("SIN GARANTIA"),
+                            SolicitudNuevoActions.plazo(plazo),
+                            SolicitudNuevoActions.nroplazo(nroplazo),
+                            SolicitudNuevoActions.opencalendar(),
+                            SolicitudNuevoActions.nextmonthcalendar(),
+                            SolicitudNuevoActions.okcalendar(),
+                            SolicitudNuevoActions.calculartasa(),
+                            SolicitudNuevoActions.guardaroperacion(),
+                            SolicitudNuevoActions.confirmaroperacion());
+
+                }
+            } catch (SQLException e) {
+                System.out.println("Error connection to the database");
+                e.printStackTrace();
+            }
         }
-
-
     }
-
-
-
 }
+
+
+
+
+
+
--- src/test/java/pe/com/bif/techcases/testautomation/bdconnections/connectionSql.java
+++ src/test/java/pe/com/bif/techcases/testautomation/bdconnections/connectionSql.java
@@ -9,6 +9,10 @@ public class connectionSql {
          // - Change bif3pricing20
          // - Change BIFPricing-
          // IntegratedSecury=true
+         String cargo ="EJECUTIVO BANCA PERSONAS SENIOR";
+         String clientebif="1";
+         String segmentacion="2";
+
 
          String connectionString =
                  "jdbc:sqlserver://bif3pricing20;Database=BIFPricing;TrustServerCertificate=true";
@@ -16,28 +20,36 @@ public class connectionSql {
          String login =
                  "usr_princing";
 
-         String password =
+         String pass =
                  "43#pr1@8";
 
-
-         String cargo = "GERENTE DE OFICINA";
          try{
-             Connection connection = DriverManager.getConnection(connectionString,login,password);
+             Connection connection = DriverManager.getConnection(connectionString,login,pass);
              System.out.println("Connection established.");
 
+
              Statement mystatement = connection.createStatement();
+             ResultSet bd = mystatement.executeQuery(
+                     "SELECT * FROM (SELECT TOP 1 REGISTRO,PASSWORD FROM ORG_INTEGRANTE2 WHERE PRESENCIA = 1 AND LOGUEO = 1 AND ESTADO = 1 AND ESTADO_SOL_ACTIVACION = 'ACTIVO' AND DESC_CARGO = '"+cargo+"' ORDER BY NEWID()\n" +
+                             ") AS T1, (SELECT TOP(1) l.num_doc_identidad as NRODOCUMENTO, I.INGRESO_NETO AS INGRESOS, D.COD_CUENTA AS NROPAGARE, D.DIVISA AS DIVISA FROM CFG_LLE L INNER JOIN CFG_CLIENTES C ON L.num_doc_identidad = C.NRO_DOCUMENTO \n" +
+                             "JOIN CFG_INGRESOS I ON I.NUM_DOC_IDENTIDAD = L.num_doc_identidad\n" +
+                             "JOIN CFG_INFOADICIONAL_DESEMBOLSOS D ON D.NRO_DOCUMENTO = L.num_doc_identidad\n" +
+                             "WHERE L.tip_doc_identidad = '1' AND L.NUM_DOC_IDENTIDAD != ' ' AND C.RANGO_INGRESO != '0.0' AND C.CLIENTE_BIF = '"+clientebif+"' AND C.SEGMENTACION_RIESGOS = '"+segmentacion+"'\n" +
+                             "AND L.cliente_bif = '"+clientebif+"' AND L.Segmentacion ='"+segmentacion+"' AND I.TIP_DOC_IDENTIDAD = '1' ORDER BY NEWID()) AS T2,\n" +
+                             "(SELECT TOP(1) OPCION AS TIPORELACIONLABORAL FROM VLR_VARIABLE WHERE VARIABLE = 'TIPO_REL_LABORAL' AND COD_PRODUCTO = '1' ORDER BY NEWID()) as T3");
 
-             ResultSet miresult = mystatement.executeQuery(
-                     "SELECT TOP 1 REGISTRO,PASSWORD FROM ORG_INTEGRANTE2 " +
-                             "WHERE PRESENCIA = 1 AND LOGUEO = 1 AND ESTADO = 1 AND ESTADO_SOL_ACTIVACION = 'ACTIVO' " +
-                             "AND DESC_CARGO = '"+cargo+"' ORDER BY NEWID()");
+             while (bd.next()) {
+                 String usuario = bd.getString(1);
+                 String password = bd.getString(2);
+                 String nrodocumento = bd.getString(3);
+                 double ingresoneto = bd.getDouble(4);
+                 String nropagare = "";
+                 String tiporelacionlaboral = bd.getString(5);
 
-             while (miresult.next()){
-                 System.out.println(miresult.getString(1)+" "+miresult.getString(2));
-
+                 System.out.println(usuario+" "+password+" "+nrodocumento+" "+ingresoneto+" "+nropagare+""+tiporelacionlaboral);
              }
-
-         } catch (SQLException e){
+         }
+             catch (SQLException e){
              System.out.println("Error connection to the database");
              e.printStackTrace();
          }
